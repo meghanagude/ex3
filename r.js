b3aa0f33d20d69a1812958abafdf2ed9601fdc59
meghanagude <meghanagude16@gmail.com>
2025-08-24 17:30:07 +0530

    branch 3 commit

--- r.js
+++ r.js
@@ -1,13 +1,13 @@
 document.getElementById("ecetForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // Prevent form from submitting
+  e.preventDefault(); 
 
   const phone = document.getElementById("phone").value;
 
   if (!/^\d{10}$/.test(phone)) {
-    alert("Phone number must be 10 digits.");
+    alert("Phone number is strictly 10 digits.");
     return;
   }
 
-  alert("Form submitted successfully!");
-  this.reset(); // Optional: reset the form after submission
+  alert("Congrats,Form submitted successfully!");
+  this.reset(); 
 });
